463b9170b65f876071592d716d9bb5f0a6309ac4
Brandon <bbrungardt02@gmail.com>
2023-04-23 17:17:12 -0500

    First Commit

---
+++ strangers-things/src/App.jsx
@@ -0,0 +1,10 @@
+import "./App.css";
+
+function App() {
+  return <div className="App">Stranger's Things</div>;
+}
+
+export default App;
+
+const COHORT_NAME = "2301-FTB-ET-WEB-AM";
+const BASE_URL = `https://strangers-things.herokuapp.com/api/${COHORT_NAME}`;
